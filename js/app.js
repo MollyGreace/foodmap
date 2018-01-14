68f2d3399890f27ce49a40ea8805acaf0fe1f18b
MollyGreace <greace.ss@gmail.com>
2018-01-13 22:27:32 -0500

    añade splash

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  
+  // splash
+  $('#second').toggle();
+  setTimeout(function() {
+    $('#first').delay().fadeOut(1000);
+    $('#second').toggle();
+  }, 3000);  
 });
