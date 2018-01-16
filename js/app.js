ca3ee3c64b20ad640e4b548b3d3bb08618b55494
MollyGreace <greace.ss@gmail.com>
2018-01-15 20:28:10 -0500

    añade fotos, funcion agrega foto de restaurantes, modifica data de restaurantes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,70 @@
 $(document).ready(function() {
+
   // splash
   $('#second').toggle();
   setTimeout(function() {
     $('#first').delay().fadeOut(1000);
     $('#second').toggle();
   }, 3000);  
+  
+  
+
+  //Seleccionamos elementos del DOM
+  var containerRestaurants = $('#container-restaurants');
+  var search = $('#search');
+  
+  //Función de cargar imágenes 
+  var loadImage = function () {
+    for (i = 0; i < data.length; i++) {
+    var photos = 
+		'<div class="col-sm-5 container">' +
+			'<img class="img-thumbnail photo-restaurants" src = ' + data[i].image + ' data-target="#restModal" data-toggle="modal" data-name= "' + data[i].name + '" data-address= "' + data[i].address + '" data-telephone= "' + data[i].telephone + '" data-web= "' + data[i].web +  
+			'<img class="img-thumbnail photo-restaurants" src = ' + data[i].back + ' data-target="#restModal" data-toggle="modal">' +
+		'</div>';
+    imagesRestaurants = containerRestaurants.append(photos);
+    };
+    return imagesRestaurants;
+  }
+  loadImage();  
+
+  // // filtro
+  // $('#search').keyup(function() {
+  //   var name = $(this).val();
+  //   $('.collection').hide();
+  //   $('.collection').each(function() {
+  //     var search = $(this).text().toLowerCase();
+  //     if (search.indexOf(name) !== -1) {
+  //       $(this).show();
+  //     }
+  //   });
+  // });
+
+  // // modal
+  // $('.content').click(function() {
+  //   var name = $(this).data('name'),
+  //   	address = $(this).data('address'),
+  //     phone = $(this).data('phone'),
+  //     attention = $(this).data('attention'),
+  //     money = $(this).data('money'),
+  //     type = $(this).data('type');
+  //   $('.name').text(name);
+  //   $('.address').text(address);
+  //   $('.phone').text(phone);
+  //   $('.attention').text(attention);
+  //   $('.money').text(money);
+  //   $('.type').text(type);
+  // });
+	
+  // $('.close').click(function() {
+  //   $('#search').val('');
+  //   $('.collection').show();
+  // });
+	
+  // // mouseover
+  // $('.collection').mouseover(function() {
+  //   $('p', this).hide();
+  // });
+  // $('.collection').mouseout(function() {
+  //   $('p', this).show();
+  // });
 });
